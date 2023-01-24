f79059559fd851e52020309204e54a8476bd3b3d
emirhanusta <emirhan1usta@gmail.com>
2023-01-25 01:06:06 +0300

    started project

--- sc-frontend/src/api/apiCalls.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const signUp =(body)=>{
-    return axios.post('/api/1.0/users',body);
-}
--- ws/src/main/java/com/socialNetwork/ws/user/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.socialNetwork.ws.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users, Integer>{
-
-	Users findByUserName(String userName);
-}
