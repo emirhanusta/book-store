867859d7cd8a28924e58943af955ca15f1ac23fc
emirhanusta <emirhan1usta@gmail.com>
2023-01-25 12:24:38 +0300

    first commit

---
+++ book-store/src/test/java/bookstore/bookstore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package bookstore.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
